c774c35271de1d1a57e925d344f8dd15b29f9856
accauntforwork <accauntforwork01@gmail.com>
2024-03-31 11:19:54 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+    </div>
+  );
+}
+
+export default App;
